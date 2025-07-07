94cf8bb093c9bc196fb77fcd79ee8dcfda4b2755
Dustin Conlon <dustin@dustinconlon.com>
2025-07-07 13:43:52 -0600

    Updated for v13
    Added translations, and fixed button alignment

--- scripts/module.js
+++ scripts/module.js
@@ -1,6 +1,10 @@
 let socket;
 let requests = [];
 
+Hooks.once("init", () => {
+  CONFIG.debug.hooks = true;
+})
+
 Hooks.once("socketlib.ready", () => {
 	socket = socketlib.registerModule("table-safety");
   
@@ -29,7 +33,7 @@ function highlightAcknowledge() {
 
 function tableSafetyPauseRequest(userId) {
   highlightAcknowledge();
-  ui.notifications.warn("TABLE SAFETY - A player has requested a game pause.", { permanent: true });
+  ui.notifications.warn(game.i18n.localize("TableSafety.MESSAGES.PauseRequest"), { permanent: true });
   requests.push({
     type: "pause",
     user: userId,
@@ -39,7 +43,7 @@ function tableSafetyPauseRequest(userId) {
 
 function tableSafetyHardstopRequest(userId) {
   highlightAcknowledge();
-  ui.notifications.warn("TABLE SAFETY - A player has requested a scene and/or situation hardstop.", { permanent: true });
+  ui.notifications.warn(game.i18n.localize("TableSafety.MESSAGES.HardstopRequest"), { permanent: true });
   requests.push({
     type: "hardstop",
     user: userId,
@@ -50,7 +54,7 @@ function tableSafetyHardstopRequest(userId) {
 function tableSafetyFastforwardRequest(userId) {
   highlightAcknowledge();
   const username = game.users.get(userId).name;
-  ui.notifications.warn(`TABLE SAFETY - A player would like to fast-forward through this scene and/or situation.`, { permanent: true });
+  ui.notifications.warn(game.i18n.localize("TableSafety.MESSAGES.FastForwardRequest"), { permanent: true });
   requests.push({
     type: "fastforward",
     user: userId,
@@ -61,7 +65,7 @@ function tableSafetyFastforwardRequest(userId) {
 function tableSafetyHandsRequest(userId) {
   highlightAcknowledge();
   const username = game.users.get(userId).name;
-  ui.notifications.warn(`TABLE SAFETY - ${username} would like a chance to speak.`, { permanent: true });
+  ui.notifications.warn(game.i18n.format("TableSafety.MESSAGES.SpeakRequest", { username }), { permanent: true });
   requests.push({
     type: "hands",
     user: userId,
@@ -71,7 +75,7 @@ function tableSafetyHandsRequest(userId) {
 
 function tableSafetyHandsNotifyAll() {
   if (!game.user.isGM) 
-    ui.notifications.warn(`TABLE SAFETY - Another player would like the chance to speak.`);
+    ui.notifications.warn(game.i18n.localize("TableSafety.MESSAGES.SpeakNotification"));
 }
 
 async function gmAcknowledgement(userId) {
@@ -90,16 +94,17 @@ async function gmAcknowledgement(userId) {
   }).render({ force: true });
 }
 
-Hooks.on("renderSidebarTab", (tab) => {
-  if (tab.title !== "Chat Log") return;
 
+Hooks.once("renderChatInput", (app, elements, context) => {
   const pauseButton = document.createElement('button');
   const pauseButtonIcon = document.createElement('i');
   pauseButtonIcon.classList.add("fa-solid", "fa-pause");
   pauseButton.appendChild(pauseButtonIcon);
   pauseButton.type = "button";
-  pauseButton.classList.add("table-safety-button")
+  pauseButton.classList.add("ui-control", "icon", "table-safety-button");
+  pauseButton.dataset.tooltip = game.i18n.localize("TableSafety.TOOLTIPS.PauseTooltip");
   pauseButton.onclick = (event) => {
+    foundry.ui.notifications.info(game.i18n.localize("TableSafety.MESSAGES.RequestSent"));
     socket.executeForAllGMs("pause", game.user._id);
   };
   
@@ -108,8 +113,10 @@ Hooks.on("renderSidebarTab", (tab) => {
   hardstopButtonIcon.classList.add("fa-solid", "fa-shield-xmark");
   hardstopButton.appendChild(hardstopButtonIcon);
   hardstopButton.type = "button";
-  hardstopButton.classList.add("table-safety-button")
+  hardstopButton.classList.add("ui-control", "icon", "table-safety-button")
+  hardstopButton.dataset.tooltip = game.i18n.localize("TableSafety.TOOLTIPS.HardstopTooltip");
   hardstopButton.onclick = (event) => {
+    foundry.ui.notifications.info(game.i18n.localize("TableSafety.MESSAGES.RequestSent"));
     socket.executeForAllGMs("hardstop", game.user._id);
   };
 
@@ -118,8 +125,10 @@ Hooks.on("renderSidebarTab", (tab) => {
   fastforwardButtonIcon.classList.add("fa-solid", "fa-forward");
   fastforwardButton.appendChild(fastforwardButtonIcon);
   fastforwardButton.type = "button";
-  fastforwardButton.classList.add("table-safety-button")
+  fastforwardButton.classList.add("ui-control", "icon", "table-safety-button")
+  fastforwardButton.dataset.tooltip = game.i18n.localize("TableSafety.TOOLTIPS.FastForwardTooltip");
   fastforwardButton.onclick = async (event) => {
+    foundry.ui.notifications.info(game.i18n.localize("TableSafety.MESSAGES.RequestSent"));
     await socket.executeForAllGMs("fastforward", game.user._id);
   };
 
@@ -128,8 +137,10 @@ Hooks.on("renderSidebarTab", (tab) => {
   handsButtonIcon.classList.add("fa-solid", "fa-hand");
   handsButton.appendChild(handsButtonIcon);
   handsButton.type = "button";
-  handsButton.classList.add("table-safety-button")
+  handsButton.classList.add("ui-control", "icon", "table-safety-button")
+  handsButton.dataset.tooltip = game.i18n.localize("TableSafety.TOOLTIPS.SpeakTooltip");
   handsButton.onclick = async (event) => {
+    foundry.ui.notifications.info(game.i18n.localize("TableSafety.MESSAGES.RequestSent"));
     await socket.executeForOthers("handsNotify");
     await socket.executeForAllGMs("hands", game.user._id);
   };
@@ -139,7 +150,8 @@ Hooks.on("renderSidebarTab", (tab) => {
   acknowledgeButtonIcon.classList.add("fa-solid", "fa-thumbs-up");
   acknowledgeButton.appendChild(acknowledgeButtonIcon);
   acknowledgeButton.type = "button";
-  acknowledgeButton.classList.add("table-safety-button", "table-safety-acknowledge")
+  acknowledgeButton.classList.add("ui-control", "icon", "table-safety-button", "table-safety-acknowledge")
+  acknowledgeButton.dataset.tooltip = game.i18n.localize("TableSafety.TOOLTIPS.AcknowledgeTooltip");
   acknowledgeButton.onclick = (event) => {
     requests.forEach(r => {
       if (r.type === "pause") game.togglePause(true, true);
@@ -157,9 +169,123 @@ Hooks.on("renderSidebarTab", (tab) => {
   if (game.user.isGM) control.appendChild(acknowledgeButton);
   control.classList.add("table-safety");
 
-  //const nodes = Array.from(tab.element[0].childNodes);
-  //const before = nodes.filter(f => f.id === "chat-controls");
-  const before = tab.element[0].childNodes[5];
+  const spacer = document.createElement("div");
+  spacer.style.width = "5px";
+  spacer.style.height = "5px";
 
-  tab.element[0].insertBefore(control, before);
-});
+  //context.previousParent.appendChild(control);
+  context.previousParent.querySelector("#roll-privacy").appendChild(spacer);
+  context.previousParent.querySelector("#roll-privacy").appendChild(control);
+  const chatScroll = app.element.querySelector(".chat-scroll");
+  chatScroll.style.height = chatScroll.style.height - "30px";
+  //app.element.querySelector("#roll-privacy").appendChild(control);
+})
+
+
+// Hooks.on("getSceneControlButtons", (sceneControls) => {
+//   sceneControls.tableSafety = {
+//     activeTool: "select",
+//     name: "Table Safety",
+//     icon: "fa-solid fa-xmark",
+//     order: 99,
+//     onChange: (event, active) => {
+//       console.log("Table Safety - onChange", event, active);
+//     },
+//     onToolChange: (event, tool) => {
+
+//     },
+//     title: "TableSafety.UI.SceneControlGroup",
+//     tools: {
+//       select: {
+//         icon: "fa-solid fa-expand",
+//         name: "select",
+//         order: 1,
+//         title: "TableSafety.UI.Select",
+//         toolClip: {}
+//       },
+//       pause: {
+//         icon: "fa-solid fa-pause",
+//         name: "pause",
+//         order: 2,
+//         title: "TableSafety.UI.Pause",
+//         toolClip: {} //! Implement toolClip for all but the 'none' button
+//       }
+//     }
+//   };
+
+//   return sceneControls;
+// })
+
+// Hooks.on("renderSidebarTab", (tab) => {
+//   if (tab.title !== "Chat Log") return;
+
+//   const pauseButton = document.createElement('button');
+//   const pauseButtonIcon = document.createElement('i');
+//   pauseButtonIcon.classList.add("fa-solid", "fa-pause");
+//   pauseButton.appendChild(pauseButtonIcon);
+//   pauseButton.type = "button";
+//   pauseButton.classList.add("table-safety-button")
+//   pauseButton.onclick = (event) => {
+//     socket.executeForAllGMs("pause", game.user._id);
+//   };
+  
+//   const hardstopButton = document.createElement('button');
+//   const hardstopButtonIcon = document.createElement('i');
+//   hardstopButtonIcon.classList.add("fa-solid", "fa-shield-xmark");
+//   hardstopButton.appendChild(hardstopButtonIcon);
+//   hardstopButton.type = "button";
+//   hardstopButton.classList.add("table-safety-button")
+//   hardstopButton.onclick = (event) => {
+//     socket.executeForAllGMs("hardstop", game.user._id);
+//   };
+
+//   const fastforwardButton = document.createElement('button');
+//   const fastforwardButtonIcon = document.createElement('i');
+//   fastforwardButtonIcon.classList.add("fa-solid", "fa-forward");
+//   fastforwardButton.appendChild(fastforwardButtonIcon);
+//   fastforwardButton.type = "button";
+//   fastforwardButton.classList.add("table-safety-button")
+//   fastforwardButton.onclick = async (event) => {
+//     await socket.executeForAllGMs("fastforward", game.user._id);
+//   };
+
+//   const handsButton = document.createElement('button');
+//   const handsButtonIcon = document.createElement('i');
+//   handsButtonIcon.classList.add("fa-solid", "fa-hand");
+//   handsButton.appendChild(handsButtonIcon);
+//   handsButton.type = "button";
+//   handsButton.classList.add("table-safety-button")
+//   handsButton.onclick = async (event) => {
+//     await socket.executeForOthers("handsNotify");
+//     await socket.executeForAllGMs("hands", game.user._id);
+//   };
+
+//   const acknowledgeButton = document.createElement('button');
+//   const acknowledgeButtonIcon = document.createElement('i');
+//   acknowledgeButtonIcon.classList.add("fa-solid", "fa-thumbs-up");
+//   acknowledgeButton.appendChild(acknowledgeButtonIcon);
+//   acknowledgeButton.type = "button";
+//   acknowledgeButton.classList.add("table-safety-button", "table-safety-acknowledge")
+//   acknowledgeButton.onclick = (event) => {
+//     requests.forEach(r => {
+//       if (r.type === "pause") game.togglePause(true, true);
+//       socket.executeAsUser("ack", r.user);
+//     });
+//     requests = [];
+//     acknowledgeButton.classList.remove("active");
+//   };
+
+//   const control = document.createElement('div');
+//   if (!game.user.isGM) control.appendChild(pauseButton);
+//   if (!game.user.isGM) control.appendChild(hardstopButton);
+//   if (!game.user.isGM) control.appendChild(handsButton);
+//   if (!game.user.isGM) control.appendChild(fastforwardButton);
+//   if (game.user.isGM) control.appendChild(acknowledgeButton);
+//   control.classList.add("table-safety");
+
+//   //const nodes = Array.from(tab.element[0].childNodes);
+//   //const before = nodes.filter(f => f.id === "chat-controls");
+//   const before = tab.element[0].childNodes[5];
+
+//   tab.element[0].insertBefore(control, before);
+// });
